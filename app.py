746bcb4cfca2689dbb2c9bc0afb15d760ac47bea
Basov Vasily (IFAG SMO PM) <vasily.basov@infineon.com>
2023-09-21 09:21:56 +0200

    another

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import time
 
 import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from waitress import serve
 
 from proxy import ProxyBox
@@ -9,21 +9,50 @@ from proxy import ProxyBox
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST', 'CONNECT'])
-def proxy():
-    # Retrieve request method, URL, headers, and body
-    print(request.method)
-    print(request.headers)
-    print(request.data)
-    method = request.method
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {key: value for (key, value) in request.headers if key != 'Host'}
-    data = request.data
-    # Make the request to the target API server using requests library
-    response = requests.request(method, url, headers=headers, data=data, stream=True)
+import os
 
-    # Return the response from the target API server
-    return jsonify(response), response.status_code
+
+# API_HOST = os.environ.get('API_HOST'); assert API_HOST, 'Envvar API_HOST is required'
+API_HOST = "https://api.openai.com/v1/chat/completions"
+
+@app.route('/', defaults={'path': ''})  # ref. https://medium.com/@zwork101/making-a-flask-proxy-server-online-in-10-lines-of-code-44b8721bca6
+@app.route('/<path>')
+def redirect_to_API_HOST(path):  #NOTE var :path will be unused as all path we need will be read from :request ie from flask import request
+    res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
+        method          = request.method,
+        url             = request.url.replace(request.host_url, f'{API_HOST}/'),
+        headers         = {k:v for k,v in request.headers if k.lower() != 'host'}, # exclude 'host' header
+        data            = request.get_data(),
+        cookies         = request.cookies,
+        allow_redirects = False,
+    )
+
+    #region exlcude some keys in :res response
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']  #NOTE we here exclude all "hop-by-hop headers" defined by RFC 2616 section 13.5.1 ref. https://www.rfc-editor.org/rfc/rfc2616#section-13.5.1
+    headers          = [
+        (k,v) for k,v in res.raw.headers.items()
+        if k.lower() not in excluded_headers
+    ]
+    #endregion exlcude some keys in :res response
+
+    response = Response(res.content, res.status_code, headers)
+    return response
+
+# @app.route('/', methods=['GET', 'POST', 'CONNECT'])
+# def proxy():
+#     # Retrieve request method, URL, headers, and body
+#     print(request.method)
+#     print(request.headers)
+#     print(request.data)
+#     method = request.method
+#     url = "https://api.openai.com/v1/chat/completions"
+#     headers = {key: value for (key, value) in request.headers if key != 'Host'}
+#     data = request.data
+#     # Make the request to the target API server using requests library
+#     response = requests.request(method, url, headers=headers, data=data, stream=True)
+#
+#     # Return the response from the target API server
+#     return jsonify(response), response.status_code
 
 
 if __name__ == '__main__':

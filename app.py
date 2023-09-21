aaf1eccc608505eb7dc2a6135bd73fb204f2e082
Basov Vasily (IFAG SMO PM) <vasily.basov@infineon.com>
2023-09-21 19:48:52 +0200

    works!!!

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ def replace_url_handler():
     url = request_data.get('url')
     payload = request_data.get('payload')
 
-    print(method, headers, url, payload)
+    # print(method, headers, url, payload)
     # Replace the URL with the new one
 
     # Send the updated request with the original method, headers, and data
     response = requests.request(method, url, headers=headers, json=payload)
 
-    print(response.json(), response.status_code, response.headers.items())
+    # print(response.json(), response.status_code, response.headers.items())
 
     # Return the response object
     # return response.json(), response.status_code, response.headers.items()
